6224a3e1eee3ad246a8ba4c87946621a25310e16
ayusuningsih <ayuusuningsih20@gmail.com>
2024-05-26 12:54:43 +0700

    View Layouts

--- app/Controllers/Pages.php
+++ app/Controllers/Pages.php
@@ -2,25 +2,43 @@
 
 namespace App\Controllers;
 
+use Stringable;
+
 class Pages extends BaseController
 {
-  public function index()
+  public function index(): String
   {
     $data = [
       'title' => 'Home | Web Programming'
     ];
-    echo view('layout/header', $data);
-    echo view('pages/home');
-    echo view('layout/footer');
+    return view('pages/home', $data);
   }
 
-  public function about()
+  public function about(): String
   {
     $data = [
       'title' => 'About Me'
     ];
-    echo view('layout/header', $data);
-    echo view('pages/about');
-    echo view('layout/footer');
+    return view('pages/about', $data);
+  }
+
+  public function contact(): String
+  {
+    $data = [
+      'title' => 'Contact',
+      'alamat' => [
+        [
+          'tipe' => 'rumah',
+          'alamat' => 'Jl. xyz No 789',
+          'kota' => 'Lhokseumawe'
+        ],
+        [
+          'tipe' => 'kantor',
+          'alamat' => 'Jl. abc No 123',
+          'kota' => 'Aceh Tamiang'
+        ]
+      ]
+    ];
+    return view('pages/contact', $data);
   }
 }
